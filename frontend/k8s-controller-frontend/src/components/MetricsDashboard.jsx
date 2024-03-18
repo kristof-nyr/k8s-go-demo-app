28e8a4dcafb93e0f5d801540a2e5fd9e8ef10d37
Kristóf Nyári <nyari.kristof66@gmail.com>
2024-03-18 15:18:40 +0100

    fix: :bug: Fixed formatting

--- frontend/k8s-controller-frontend/src/components/MetricsDashboard.jsx
+++ frontend/k8s-controller-frontend/src/components/MetricsDashboard.jsx
@@ -4,46 +4,47 @@ import { transformKubernetesNodeMetrics } from "../helpers/json-helpers";
 import "../styles/metrics.css";
 
 const MetricsDashboard = () => {
-    const [metricsData, setMetricsData] = useState({});
+  const [metricsData, setMetricsData] = useState({});
 
-    useEffect(() => {
-        getKubernetesNodeMetrics().then((data) => {
-            setMetricsData(data);
-        });
-    }, []);
+  useEffect(() => {
+    getKubernetesNodeMetrics().then((data) => {
+      setMetricsData(data);
+    });
+  }, []);
 
-    const transformedMetrics = transformKubernetesNodeMetrics(metricsData);
+  const transformedMetrics = transformKubernetesNodeMetrics(metricsData);
 
-    return (
-        <div className="component-metrics-dashboard-container">
-            <h1 className="component-metrics-dashboard-title">Kubernetes Node Metrics</h1>
-            <table className="component-metrics-dashboard-metrics-table">
-                <thead>
-                    <tr>
-                        <th className="table-header">Node Name</th>
-                        <th className="table-header">Operating System</th>
-                        <th className="table-header">OS Architecture</th>
-                        <th className="table-header">CPU Usage</th>
-                        <th className="table-header">RAM Usage</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {transformedMetrics.map((metric, index) => {
-                        return (
-                            <tr key={index}>
-                                <td>{metric.nodeName}</td>
-                                <td>{metric.operatingSystem}</td>
-                                <td>{metric.osArchitecture}</td>
-                                <td>{metric.cpuUsage}</td>
-                                <td>{metric.memoryUsage}</td>
-                            </tr>
-                        )
-                    })
-                    }
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div className="component-metrics-dashboard-container">
+      <h1 className="component-metrics-dashboard-title">
+        Kubernetes Node Metrics
+      </h1>
+      <table className="component-metrics-dashboard-metrics-table">
+        <thead>
+          <tr>
+            <th className="table-header">Node Name</th>
+            <th className="table-header">Operating System</th>
+            <th className="table-header">OS Architecture</th>
+            <th className="table-header">CPU Usage</th>
+            <th className="table-header">RAM Usage</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transformedMetrics.map((metric, index) => {
+            return (
+              <tr key={index}>
+                <td>{metric.nodeName}</td>
+                <td>{metric.operatingSystem}</td>
+                <td>{metric.osArchitecture}</td>
+                <td>{metric.cpuUsage}</td>
+                <td>{metric.memoryUsage}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default MetricsDashboard;
--- frontend/k8s-controller-frontend/src/helpers/json-helpers.js
+++ frontend/k8s-controller-frontend/src/helpers/json-helpers.js
@@ -1,22 +1,19 @@
-
-
 const transformKubernetesNodeMetrics = (data) => {
-    let metricsObject = []
-    // Handle pre-hyrdated content rendering
-    // The case when the raw html is first rendered, afterwards the page will get hydrated with by JS
-    if (data.items) {
-    data.items.forEach(metricItem => {
-        metricsObject.push({
-            nodeName: metricItem.metadata.name,
-            operatingSystem: metricItem.metadata.labels["kubernetes.io/os"],
-            osArchitecture: metricItem.metadata.labels["kubernetes.io/arch"],
-            cpuUsage: metricItem.usage.cpu,
-            memoryUsage: metricItem.usage.memory
-        })
-    })}
-    return metricsObject;
-}
+  let metricsObject = [];
+  // Handle pre-hyrdated content rendering
+  // The case when the raw html is first rendered, afterwards the page will get hydrated with by JS
+  if (data.items) {
+    data.items.forEach((metricItem) => {
+      metricsObject.push({
+        nodeName: metricItem.metadata.name,
+        operatingSystem: metricItem.metadata.labels["kubernetes.io/os"],
+        osArchitecture: metricItem.metadata.labels["kubernetes.io/arch"],
+        cpuUsage: metricItem.usage.cpu,
+        memoryUsage: metricItem.usage.memory,
+      });
+    });
+  }
+  return metricsObject;
+};
 
-export {
-    transformKubernetesNodeMetrics
-}
+export { transformKubernetesNodeMetrics };
