a09777460c0de4053f1ddb1398849e5168ea6bc1
Kristóf Nyári <nyari.kristof66@gmail.com>
2024-03-18 15:17:16 +0100

    feat: :bug: Added helper functions to communicate with backend

---
+++ frontend/k8s-controller-frontend/src/helpers/json-helpers.js
@@ -0,0 +1,22 @@
+
+
+const transformKubernetesNodeMetrics = (data) => {
+    let metricsObject = []
+    // Handle pre-hyrdated content rendering
+    // The case when the raw html is first rendered, afterwards the page will get hydrated with by JS
+    if (data.items) {
+    data.items.forEach(metricItem => {
+        metricsObject.push({
+            nodeName: metricItem.metadata.name,
+            operatingSystem: metricItem.metadata.labels["kubernetes.io/os"],
+            osArchitecture: metricItem.metadata.labels["kubernetes.io/arch"],
+            cpuUsage: metricItem.usage.cpu,
+            memoryUsage: metricItem.usage.memory
+        })
+    })}
+    return metricsObject;
+}
+
+export {
+    transformKubernetesNodeMetrics
+}
---
+++ frontend/k8s-controller-frontend/src/helpers/kubernetes-helpers.js
@@ -0,0 +1,81 @@
+import axios from "axios";
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+
+/**
+ * Getter function for non-namespaced resources such as Nodes or Namespaces
+ *
+ * @param {string} resource
+ * @returns {Promise} JSON object of the requested resource
+ */
+const getKubernetesResources = async (resource) => {
+  const response = await axios.get(`${BACKEND_URL}/api/v1/${resource}`);
+  return response.data;
+};
+
+/**
+ * Getter function for namespaced resources such as Pods or Deployments
+ *
+ * @param {string} resource
+ * @param {string} namespace
+ * @returns {Promise} JSON object of the requested resource
+ */
+
+const getKubernetesNamespacedResources = async (resource, namespace) => {
+  const response = await axios.get(
+    `${BACKEND_URL}/api/v1/namespaces//${namespace}/${resource}`,
+  );
+  return response.data;
+};
+
+/**
+ * Getter function for a specific namespaced resource
+ *
+ * @param {string} resource
+ * @param {string} namespace
+ * @param {string} resourceName
+ * @returns {Promise} JSON object of the requested resource
+ */
+const getKubernetesNamespacedResourcesInfo = async (
+  resource,
+  namespace,
+  resourceName,
+) => {
+  const response = await axios.get(
+    `${BACKEND_URL}/api/v1/namespaces/${namespace}/${resource}/${resourceName}`,
+  );
+  return response.data;
+};
+
+/**
+ * Getter function for node metrics
+ *
+ * @returns {Promise} JSON object of the requested resource
+ */
+const getKubernetesNodeMetrics = async () => {
+  const response = await axios.get(
+    `${BACKEND_URL}/api/metrics.k8s.io/v1beta1/nodes`,
+  );
+  return response.data;
+};
+
+/**
+ * Getter function for pod metrics
+ *
+ * @param {string} namespace
+ * @returns {Promise} JSON object of the requested resource
+ */
+const getKubernetesPodMetrics = async (namespace) => {
+  const response = await axios.get(
+    `${BACKEND_URL}/api/metrics.k8s.io/v1beta1/namepsaces/${namespace}/pods`,
+  );
+  return response.data;
+};
+
+export {
+  getKubernetesResources,
+  getKubernetesNamespacedResources,
+  getKubernetesNamespacedResourcesInfo,
+  getKubernetesNodeMetrics,
+  getKubernetesPodMetrics,
+};
